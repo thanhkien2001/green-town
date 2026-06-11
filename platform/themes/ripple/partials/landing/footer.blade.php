64b65a105d9f34dbdeb3f9d3c478715852d0c595
Victor Kim <nguyenkimchi10112003@email.com>
2026-06-11 21:58:35 +0700

    theme option footer

--- platform/themes/ripple/partials/landing/footer.blade.php
+++ platform/themes/ripple/partials/landing/footer.blade.php
@@ -16,18 +16,29 @@
     <div class="ldp-footer-col">
         <h3 class="ldp-footer-title">THÔNG TIN DỰ ÁN</h3>
         <ul class="ldp-footer-list">
+            @if(theme_option('ldp_footer_address', 'Lô 5, khu đô thị Vĩnh Lộc, phường Bình Tân, TP. Hồ Chí Minh'))
             <li>
                 <i class="fas fa-map-marker-alt"></i>
-                <span>Lô 5, khu đô thị Vĩnh Lộc, phường Bình Tân, TP. Hồ Chí Minh</span>
+                <span>{{ theme_option('ldp_footer_address', 'Lô 5, khu đô thị Vĩnh Lộc, phường Bình Tân, TP. Hồ Chí Minh') }}</span>
             </li>
+            @endif
+            @if(theme_option('ldp_footer_email'))
+            <li>
+                <i class="fas fa-envelope"></i>
+                <span>{{ theme_option('ldp_footer_email') }}</span>
+            </li>
+            @else
             <li>
                 <i class="fas fa-envelope"></i>
                 <span></span>
             </li>
+            @endif
+            @if(theme_option('ldp_footer_phone', '+84.97.785.07.50'))
             <li>
                 <i class="fas fa-phone-alt"></i>
-                <span>+84.97.785.07.50</span>
+                <span>{{ theme_option('ldp_footer_phone', '+84.97.785.07.50') }}</span>
             </li>
+            @endif
         </ul>
     </div>
 
@@ -35,8 +46,12 @@
     <div class="ldp-footer-col">
         <h3 class="ldp-footer-title">THEO DÕI DỰ ÁN</h3>
         <div class="ldp-footer-socials">
-            <a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a>
-            <a href="#" target="_blank"><i class="fab fa-youtube"></i></a>
+            @if(theme_option('ldp_footer_facebook', '#'))
+                <a href="{{ theme_option('ldp_footer_facebook', '#') }}" target="_blank"><i class="fab fa-facebook-f"></i></a>
+            @endif
+            @if(theme_option('ldp_footer_youtube', '#'))
+                <a href="{{ theme_option('ldp_footer_youtube', '#') }}" target="_blank"><i class="fab fa-youtube"></i></a>
+            @endif
         </div>
     </div>
 </footer>
