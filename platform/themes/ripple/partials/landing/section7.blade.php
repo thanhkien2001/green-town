93bf4b88958ad8cbc91a4e3d1b8c6f5231bd9e75
Kien2021 | apikey:ce105b6d75ee95d8a254486e6725a2666b0ee57883377bc65a414ff82b4ccd4e <thanhkien2021008@gmail.com>
2026-06-11 14:11:54 +0700

    save

--- platform/themes/ripple/partials/landing/section7.blade.php
+++ platform/themes/ripple/partials/landing/section7.blade.php
@@ -5,14 +5,14 @@
 
 <section id="section7" class="kia-section-7" style="background-color: #ffffff; position: relative; z-index: 2;">
     <!-- Leaf Decoration Top Left -->
-    <div class="s7-leaf-bg"></div>
+    <div class="s7-leaf-bg js-reveal" style="transition-delay: 400ms;"></div>
 
     <!-- Text content aligned with section 6 -->
     <div class="s7-content-wrapper text-center">
         <h2 class="s7-heading1 font-selecta-bold js-reveal">{!! nl2br(e($heading1)) !!}</h2>
     </div>
 
-    <div class="s7-image-wrapper text-center js-reveal" style="transition-delay: 200ms;">
+    <div class="s7-image-wrapper text-center js-reveal" style="transition-delay: 600ms;">
         <img src="{{ RvMedia::getImageUrl($floorPlanImg, null, false, RvMedia::getDefaultImage()) }}" alt="Mặt bằng" class="s7-floor-plan-img" />
     </div>
 </section>
